5d2bed64b6abee74f252d3b754499146e64919bd
Sagar Joshi <70212860+ssj-8921@users.noreply.github.com>
2022-06-10 09:46:01 -0700

    Updates and new files
    Introduced `script.csh` that runs the timing analysis code on sliced versions of the main data in order to compute variation of fwhm in a single run using `rms.C`. Accordingly modified `mpmt_timing_analysis.cpp` to accommodate that and made some other minor edits.

--- mpmt-analysis/timing-analysis/mpmt_timing_analysis.cpp
+++ mpmt-analysis/timing-analysis/mpmt_timing_analysis.cpp
@@ -11,9 +11,13 @@
 #include "TProfile.h"
 
 #include <iostream>
+#include <fstream>
 #include <vector>
+
+///////////////////// defining the Fit functions for the tts////////////////////////////
+
 double funcEMG(double *x, double *p){
-  //Exponential gaussian used for fitting
+  //exponential gaussian used for Fitting
   // p[0]: amplitude
   // p[1]: gaussian mu
   // p[2]: gaussian sig
@@ -28,14 +32,14 @@ double funcEMG(double *x, double *p){
 }
 
 double bessel(double *x, double *p){
-  //Exponential gaussian used for fitting
+  //exponential gaussian used for Fitting
   double xx = (x[0] - p[1]) * p[0];
 
   double y;
   if(x[0] < p[1]){
     y = p[3];
   }else{
-    //y = p[3] + p[2] * (TMath::Sin(xx) / (xx * xx) - TMath::Cos(xx)/xx); 
+    //y = p[3] + p[2] * (TMath::Sin(xx) / (xx * xx) - tmath::cos(xx)/xx); 
     y = p[3] + p[2] / pow(xx,p[4]) *
       ((15/(xx*xx*xx) - 6/xx) * TMath::Sin(xx) / (xx) - ((15/(xx*xx) -1) *TMath::Cos(xx)/xx) ); 
   }
@@ -43,26 +47,26 @@ double bessel(double *x, double *p){
   return y ;
 }
 
-
+///////////////////////////////////////////////////////////////////////////
 int main( int argc, char* argv[] ) {
 
   if ( argc != 2 ){
-    std::cerr<<"Usage: ptf_ttree_analysis.app ptf_analysis.root\n";
+    std::cerr<<"Usage: ptf_TTree_analysis.app ptf_analysis.root\n";
     exit(0);
   }
 
   //TFile * fin = new TFile( argv[1], "update" );
   TFile * fin = new TFile( argv[1], "read" );
 
-
-  // get the waveform fit TTree
+  ///////////////////////Getting the TTrees/////////////////////////////////////
+  // Get the waveform Fit TTree
   std::cout << "TTree 0" << std::endl;
-  TTree * tt0 = (TTree*)fin->Get("ptfanalysis0");
+  TTree * tt0 = (TTree*)fin->Get("ptfanalysis0"); // mpmt signal
   WaveformFitResult * wf0 = new WaveformFitResult;
   if(tt0) wf0->SetBranchAddresses( tt0 );
 
   std::cout << "TTree 1" << std::endl;
-  TTree * tt1 = (TTree*)fin->Get("ptfanalysis1");
+  TTree * tt1 = (TTree*)fin->Get("ptfanalysis1"); // injected pulse
   WaveformFitResult * wf1 = new WaveformFitResult;
   wf1->SetBranchAddresses( tt1 );
 
@@ -74,7 +78,7 @@ int main( int argc, char* argv[] ) {
 
   TTree * tt18 = (TTree*)fin->Get("ptfanalysis18");
   WaveformFitResult * wf18 = new WaveformFitResult;
-  wf18->SetBranchAddresses( tt18 );
+  if(tt18)wf18->SetBranchAddresses( tt18 );
   std::cout << "TTree done" << std::endl;
 
   TTree * tt19 = (TTree*)fin->Get("ptfanalysis19");
@@ -82,48 +86,59 @@ int main( int argc, char* argv[] ) {
   if(tt19) wf19->SetBranchAddresses( tt19 );
   std::cout << "TTree done" << std::endl;
 
-  TH1F *tdiff = new TH1F("time diff","Ch 0 minus Ch 1 time difference",100,-5,1);
-  TH1F *tdiff0 = new TH1F("time diff0","PMT0 Time relative to Trigger Time",200,316,326);
-  TH1F *tdiff1 = new TH1F("time diff1","PMT1 Time relative to Trigger Time",100,80,90);
-  //  TH1F *tdiff1 = new TH1F("time diff1","PMT1 Time relative to Trigger Time",200,70,80);
+  /////////////////histograms to calculate the time difference (transit time)/////////////////////
+  TH1F *tdiff = new TH1F("time diff","ch 0 minus ch 1 time difference",100,-5,1);
+  TH1F *tdiff0 = new TH1F("time diff0","pmt0 time relative to trigger time",200,316,326);
+  TH1F *tdiff1 = new TH1F("time diff1","pmt0 time relative to trigger time",100,54,60); //42,52
+  //  TH1F *tdiff1 = new TH1F("time diff1","pmt1 time relative to trigger time",200,70,80);
   TH1F *tdiff2 = new TH1F("time diff2","timediff2",800,-6,1);
-  TH1F *tdiff_inj = new TH1F("time diff inj","Time difference injected pulses",200,-9.3,-8.8);
+  TH1F *tdiff_inj = new TH1F("time diff inj","time difference injected pulses",200,-9.3,-8.8);
 
 
-  TH1F *tdiff00 = new TH1F("time diff00","PMT1 Time relative to Trigger Time",50,325,333);
-  TH1F *tdiff01 = new TH1F("time diff01","PMT1 Time relative to Trigger Time",50,325,333);
-  TH1F *tdiff02 = new TH1F("time diff02","PMT1 Time relative to Trigger Time",50,325,333);
-  TH1F *tdiff03 = new TH1F("time diff03","PMT1 Time relative to Trigger Time",50,325,333);
+  TH1F *tdiff00 = new TH1F("time diff00","pmt1 time relative to trigger time",50,325,333);
+  TH1F *tdiff01 = new TH1F("time diff01","pmt1 time relative to trigger time",50,325,333);
+  TH1F *tdiff02 = new TH1F("time diff02","pmt1 time relative to trigger time",50,325,333);
+  TH1F *tdiff03 = new TH1F("time diff03","pmt1 time relative to trigger time",50,325,333);
 
   TH1F *tdiff_phase[8];
   for (int i = 0; i < 8; i++){
     char name1[100];
     sprintf(name1,"time diff phase %i",i);
-    tdiff_phase[i]  = new TH1F(name1,"PMT1 Time relative to Trigger Time - phase",100,91.5,93.5);
+    tdiff_phase[i]  = new TH1F(name1,"pmt1 time relative to trigger time - phase",100,91.5,93.5);
   }
 
   TH1F *tdiff_phase_inj[8];
   for (int i = 0; i < 8; i++){
     char name1[100];
     sprintf(name1,"time diff phase %i inj",i);
-    tdiff_phase_inj[i]  = new TH1F(name1,"Time difference injected pulses - by phase",200,-9.3,-8.8);
+    tdiff_phase_inj[i]  = new TH1F(name1,"time difference injected pulses - by phase",200,-9.3,-8.8);
   }
 
 
-  TH2F *tdiff_vs_ph = new TH2F("tdiff_vs_ph","Time difference vs pulse height",40,0,0.000488/0.018*80,50,70, 80);
-  TProfile *tdiff_vs_ph_prof = new TProfile("tdiff_vs_ph_prof", "Time difference vs pulse height - Profile", 20,0,0.000488/0.018*80);
+  TH2F *tdiff_vs_ph = new TH2F("tdiff_vs_ph","time difference vs pulse height",40,0,0.000488/0.018*80,50,70, 80);
+  TProfile *tdiff_vs_ph_prof = new TProfile("tdiff_vs_ph_prof", "time difference vs pulse height - profile", 20,0,0.000488/0.018*80);
   
   TH1F *ph[2];
-  ph[0] = new TH1F("PH0","Pulse Heights ",120,0,0.000488/0.018*120);
-  ph[1] = new TH1F("PH1","Pulse Heights",2000,0,0.000488/0.018*2000);
-  std::cout << "Looping tree " << tt0->GetEntries() << " " << tt1->GetEntries() << std::endl;
-  int total_hits0 = 0, success_fits0 = 0;
-  int total_hits1 = 0, success_fits1 = 0;
+  ph[0] = new TH1F("ph0","pulse heights",360,0,0.000488/0.05685219014*360);
+  ph[1] = new TH1F("ph1","pulse heights",2000,0,0.000488/0.018*2000);//1 adc count == 0.00048v, 1 adc count == 0.48/0.018 mpe, 0.48 mv == 26 mpe => 1 pe == 18 mv 
+  std::cout << "looping tree " << tt0->GetEntries() << " " << tt1->GetEntries() << std::endl;
+  int total_hits0 = 0, success_Fits0 = 0;
+  int total_hits1 = 0, success_Fits1 = 0;
 
   int total_pe = 0.0;
   int total_nohits = 0.0;
-  // Loop the first scan point and print something 
-  for(int i = 0; i < tt0->GetEntries()-1; i++){
+  int input, slices, runNo;
+  std::cout << "input iteration :";
+  std::cin >> input;
+  std::cout << std::endl;
+  std::cout << "Divide into how many slices?";
+  std::cin >> slices;
+  std::cout << std::endl;
+  std::cout << "Enter Run Number";
+  std::cin >> runNo;
+  std::cout << std::endl;
+  // loop the first scan point and print something 
+  for(int i = input*tt0->GetEntries()/slices; i < (input+1)*tt0->GetEntries()/slices-1; i++){
     //for(int i = 0; i < 50000; i++){
     tt0->GetEvent(i );
     tt1->GetEvent(i );
@@ -132,7 +147,7 @@ int main( int argc, char* argv[] ) {
     if(tt18) tt18->GetEvent(i );
     if(tt19) tt19->GetEvent(i );
 
-    // Find the pulses in list of pulses
+    // find the pulses in list of pulses
     double pulse_time[2] = {-1,-1};
     double pulse_height[2] {-1,-1};
 
@@ -144,14 +159,14 @@ int main( int argc, char* argv[] ) {
         //if(wf->pulseTimes[k] > 2420 && wf->pulseTimes[k] < 2480){ // look for laser pulse
         if(wf->pulseTimes[k] > 2020 && wf->pulseTimes[k] < 2240){ // look for laser pulse
           pulse_time[j] = wf->pulseTimes[k];
-          if(j == 1 && 0) std::cout << "Found " << i << " " << wf->pulseTimes[k] << " "
+          if(j == 0 && 0) std::cout << "found " << i << " " << wf->pulseTimes[k] << " "
                                << (wf->pulseCharges[k])/0.018 << std::endl;
 
           //pulse_height[j] = (baseline[j] - wf->pulseCharges[k])/0.01;
           //pulse_height[j] = (wf->pulseCharges[k])/0.018 ;
-          pulse_height[j] = (wf->pulseCharges[k])/0.018 ;
+          pulse_height[j] = (wf->pulseCharges[k])/0.05685219014;
           //pulse_height[j] = (wf->pulseCharges[k])/0.016;
-          //          std::cout << "Pulse Charge: " << wf->pulseCharges[k] << std::endl;
+          //          std::cout << "pulse charge: " << wf->pulseCharges[k] << std::endl;
         }
       }
     }
@@ -170,8 +185,8 @@ int main( int argc, char* argv[] ) {
     if(pulse_height[1] > 8.5 && pulse_height[1] < 9.5) total_pe += 9.0;
     if(pulse_height[1] > 9.5 && pulse_height[1] < 10.5) total_pe += 10.0;
    
-    double time0 = wf0->mean;
-    double time1 = wf1->mean;
+    double time0 = wf0->mean; // Fitted time for pmt
+    double time1 = wf1->mean;// Fitted time for injected pulsex
     double time16 = 0.0;//wf16->mean;
     double time17 = wf17->mean;
     double time18 = wf18->mean;
@@ -180,17 +195,17 @@ int main( int argc, char* argv[] ) {
     double cfd_time1 = 0.0;//wf18->sinw;
     double time_diff = 0.0;//time16 - time18;
 
-    if(1 &&  i < 1000)std::cout << i << " Times: " << time18 << " "
+    if(1 &&  i < 1000)std::cout << i << " times: " << time0 << " "
               << time1 << " "
-              << time1 - time18 << " "
-              << time1 - time18 -75.6<< " "
+              << time1 - time0 << " "
+              << time1 - time0 -75.6<< " "
               << pulse_height[0] << " "
               << pulse_height[1] << " "
               << cfd_time0 << " " 
               << cfd_time1 << " " 
               << std::endl;
 
-    // Ch 0 to trigger
+    // ch 0 to trigger
     //    if(pulse_height[0] < 1.5 && pulse_height[0] > 0.5){  tdiff0->Fill(time0-time16 + 1.5); }
 
     //    if(pulse_height[0] < 1.5 && pulse_height[0] > 1.25){  tdiff0->Fill(time0-time16); }
@@ -198,7 +213,7 @@ int main( int argc, char* argv[] ) {
       double mtdiff = time0-time18;
       tdiff0->Fill(mtdiff);
       total_hits0++;
-      if(mtdiff > 312 && mtdiff < 330) {success_fits0++;}
+      if(mtdiff > 312 && mtdiff < 330) {success_Fits0++;}
     }
     
     double td = time1-time18;
@@ -212,10 +227,10 @@ int main( int argc, char* argv[] ) {
     tdiff_vs_ph->Fill(pulse_height[1],td);
     if(td > 327.5 && td < 330){
       tdiff_vs_ph_prof->Fill(pulse_height[1],td);
-    }// Ch 1 to trigger
+    }// ch 1 to trigger
 
     int phase = ((int)(time18-0.5)) % 8 ;
-    if(pulse_height[1] < 32.5 && pulse_height[1] > 28.5){
+    if(pulse_height[0] < 3.5 && pulse_height[0] > 0.5){
 
       double extra = 0.0;
       if(phase == 0) extra = 0.16;
@@ -227,24 +242,24 @@ int main( int argc, char* argv[] ) {
       if(phase == 6) extra = 0.07;
       if(phase == 7) extra = 0.18;
 
-      double mtdiff = time1-time18;
+      double mtdiff = time0-time1;
       //double mtdiff = time1-time18 - extra;
 
       //      if(phase == 2 || phase==3)
       //if(phase != 6)
-        tdiff1->Fill(mtdiff);
+      tdiff1->Fill(mtdiff);
 
       tdiff_phase[phase]->Fill(mtdiff);
 
       total_hits1++;
-      if(mtdiff > 60 && mtdiff < 340) {success_fits1++;}
+      if(mtdiff > 60 && mtdiff < 340) {success_Fits1++;}
     
 
     }
 
     // check injected pulse timing
     double tdiff_i = time1-time17;
-    //std::cout << "Injected: " << tdiff_i << std::endl;
+    //std::cout << "injected: " << tdiff_i << std::endl;
     tdiff_inj->Fill(tdiff_i);
     tdiff_phase_inj[phase]->Fill(tdiff_i);
       
@@ -257,13 +272,13 @@ int main( int argc, char* argv[] ) {
       if(i < 1000 && 1 && (time16-time18) > -1.25 &&
          (time16-time18) < -1.15 
          ){
-        std::cout << i << " Time difference: " << time16 << " - " << time18
+        std::cout << i << " time difference: " << time16 << " - " << time18
                   << " : " << time16-time18 << std::endl;
-        std::cout << "Charge: " << wf0->amp << " " << wf1->amp << " "
+        std::cout << "charge: " << wf0->amp << " " << wf1->amp << " "
                   << pulse_time[0] << " " << pulse_time[1] << " "
                   << pulse_height[0] << " " << pulse_height[1] << " "
                   << std::endl;
-        std::cout << "Ratio "
+        std::cout << "ratio "
                   << wf0->amp/pulse_height[0] << " " 
                   << wf1->amp/pulse_height[1] << " " << std::endl;
         
@@ -271,30 +286,30 @@ int main( int argc, char* argv[] ) {
     }     
   } 
 
-  std::cout << "Successful fit (chan 0) = " << success_fits0 << " / " << total_hits0
-            << " : " << (((double) success_fits0) /((double)total_hits0) * 100.0) << "%" << std::endl;
-  std::cout << "Successful fit (chan 1) = " << success_fits1 << " / " << total_hits1
-            << " : " << (((double) success_fits1) /((double)total_hits1) * 100.0) << "%" << std::endl;
+  std::cout << "successful Fit (chan 0) = " << success_Fits0 << " / " << total_hits0
+            << " : " << (((double) success_Fits0) /((double)total_hits0) * 100.0) << "%" << std::endl;
+  std::cout << "successful Fit (chan 1) = " << success_Fits1 << " / " << total_hits1
+            << " : " << (((double) success_Fits1) /((double)total_hits1) * 100.0) << "%" << std::endl;
 
-  std::cout << "Total events " << tt0->GetEntries() << " total PE " << total_pe << std::endl;
-  std::cout << "Mean: " << total_pe/(double)tt0->GetEntries() << std::endl;
-  std::cout << "P(0) meas: " << total_nohits/(double)tt0->GetEntries() << std::endl;
+  std::cout << "total events " << tt0->GetEntries() << " total pe " << total_pe << std::endl;
+  std::cout << "mean: " << total_pe/(double)tt0->GetEntries() << std::endl;
+  std::cout << "p(0) meas: " << total_nohits/(double)tt0->GetEntries() << std::endl;
   double lambda = total_pe/(double)tt0->GetEntries();
-  double calc_p0 = exp(-lambda);// https://en.wikipedia.org/wiki/Poisson_distribution#Poisson_Approximation
-  std::cout << "P(0) calc: " << calc_p0 << std::endl;
+  double calc_p0 = exp(-lambda);// https://en.wikipedia.org/wiki/poisson_distribution#poisson_approximation
+  std::cout << "p(0) calc: " << calc_p0 << std::endl;
   double better_lambda = - log(total_nohits/(double)tt0->GetEntries());
-  std::cout << "Better lamba: " << better_lambda << std::endl;
+  std::cout << "better lamba: " << better_lambda << std::endl;
 
     
-    TCanvas *c = new TCanvas("C");
+    TCanvas *c = new TCanvas("c");
 
   tdiff->Draw();
   //TF1 *gaus = new TF1("gaus","gaus",-5,2);
 
-  //tdiff->Fit("gaus","R");
+  //tdiff->Fit("gaus","r");
   tdiff->SetLineColor(2);
-  //tdiff2->Draw("SAME");
-  //tdiff->Fit("gaus","R");
+  //tdiff2->Draw("same");
+  //tdiff->Fit("gaus","r");
   tdiff->SetXTitle("time difference (ns)"); 
 
   gStyle->SetOptFit(1111);
@@ -302,12 +317,12 @@ int main( int argc, char* argv[] ) {
 
   
   c->SaveAs("tdiff.png");
-
-  TCanvas *c1 = new TCanvas("C1");
+  if(0){
+  TCanvas *c1 = new TCanvas("c1");
   tdiff0->Draw();
   //TF1 *gaus = new TF1("gaus","gaus",321.5,324);
   TF1 *gaus1 = new TF1("gaus1","gaus",319.5,321.5);
-  tdiff0->Fit("gaus1","R");
+  tdiff0->Fit("gaus1","r");
   tdiff0->SetXTitle("time difference (ns)"); 
 
   gStyle->SetOptFit(1111);
@@ -331,11 +346,11 @@ int main( int argc, char* argv[] ) {
 
   
   c1->SaveAs("tdiff_injected0.png");
-  
-  TCanvas *c2 = new TCanvas("C2");
+  }
+  TCanvas *c2 = new TCanvas("c2");
   tdiff1->Draw();
   //TF1 *gaus = new TF1("gaus","gaus",321.5,324);
-    TF1 *gaus2 = new TF1("gaus2","gaus",85.5,86.5);
+  TF1 *gaus2 = new TF1("gaus2","gaus",55.35,56.8);//45.5,48
   //  TF1 *gaus2 = new TF1("gaus2","gaus",74,75.6);
   tdiff1->Fit("gaus2","R");
   tdiff1->SetXTitle("time difference (ns)"); 
@@ -352,17 +367,35 @@ int main( int argc, char* argv[] ) {
     
   }
   double fwhm = end1-start1;
-  std::cout << "mean1 : " << mean1 << " " << hm1 << " " 
-            << start1 << " "
-            << end1 << " "
-            << fwhm
-            << " " << tdiff1->GetRMS() << " " 
-            << std::endl;
   
+  int binmax = ph[0]->GetMaximumBin();
+  double x = ph[0]->GetXaxis()->GetBinCenter(binmax);
+  int bin1 = tdiff1->FindFirstBinAbove(tdiff1->GetMaximum()/2);
+  int bin2 = tdiff1->FindLastBinAbove(tdiff1->GetMaximum()/2);
+  double fwhm_hist = tdiff1->GetBinCenter(bin2) - tdiff1->GetBinCenter(bin1);
 
-  c2->SaveAs("tdiff_injected1.png");
+  std::ofstream file1;
+  if (input==0) file1.open("TimingResolutionRecord_" + TString::Itoa(runNo,10) + ".txt");
+  else file1.open("TimingResolutionRecord_" + TString::Itoa(runNo,10) + ".txt", std::ios::app);
+  file1 << "mean1 : " << mean1 << " " << hm1 << " " 
+            << start1 << " "
+            << end1 << ". FWHM from Fit is "
+            << fwhm << " and from histogram is "
+            << fwhm_hist
+            << "ns   !! " << tdiff1->GetRMS() << " "
+            << std::endl << "Pulse height hist peaks at " << x 
+            << std::endl;
+  file1.close();
 
+  std::ofstream file2;
+  if (input==0) file2.open("TimingData_" + TString::Itoa(runNo,10) + ".txt");
+  else file2.open("TimingData_" + TString::Itoa(runNo,10) + ".txt", std::ios::app);
+  file2 << fwhm << std::endl;
+  file2.close();
 
+  c2->SaveAs("tdiff_injected1_run_" + TString::Itoa(runNo,10) + "_slice_" + TString::Itoa(input,10) + ".png");
+
+  if(0){
 
   TCanvas *c2inj = new TCanvas("C2inj");
   tdiff_inj->Draw();
@@ -393,10 +426,10 @@ int main( int argc, char* argv[] ) {
 
   c2inj->SaveAs("tdiff_injected_injected.png");
 
-
+  }
   TCanvas *c3 = new TCanvas("C3");
-  ph[1]->Draw();
-  ph[1]->SetXTitle("Pulse height (PE)");
+  ph[0]->Draw();
+  ph[0]->SetXTitle("Pulse height (PE)");
 
   //  ph[1]->Draw("SAME");
   //ph[1]->SetLineColor(2);
@@ -407,7 +440,7 @@ int main( int argc, char* argv[] ) {
   leg->Draw("SAME");    
   
   c3->SaveAs("pulse_heights.png");
-
+  if(0){
 
   TCanvas *c4 = new TCanvas("C4");
 
@@ -420,18 +453,18 @@ int main( int argc, char* argv[] ) {
     ch0->GetXaxis()->SetRangeUser(2000,2450);
 
 
-    TF1 *ffitfunc1 = new TF1("mygauss1",bessel,2100,2300,5);
+    TF1 *fFitfunc1 = new TF1("mygauss1",bessel,2100,2300,5);
     
 
     
-    ffitfunc1->SetParameter(0, 0.12);
-    ffitfunc1->SetParameter(1, 2200 );
-    ffitfunc1->SetParameter(2, -0.3 );
-    ffitfunc1->FixParameter(3, 0.996);
-    ffitfunc1->SetParameter(4, 1.4 );
+    fFitfunc1->SetParameter(0, 0.12);
+    fFitfunc1->SetParameter(1, 2200 );
+    fFitfunc1->SetParameter(2, -0.3 );
+    fFitfunc1->FixParameter(3, 0.996);
+    fFitfunc1->SetParameter(4, 1.4 );
 
        
-    ch0->Fit( ffitfunc1, "", "", 2170,2266);
+    ch0->Fit( fFitfunc1, "", "");
 
     
     
@@ -540,7 +573,7 @@ int main( int argc, char* argv[] ) {
   legp2->Draw("SAME");
 
   c9->SaveAs("tdiff_vs_phase_inj.png");
-  
+  }
   tdiff0->Write();
   fin->Close();
   return 0;
---
+++ mpmt-analysis/timing-analysis/rms.C
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+#include <TApplication.h>
+#include "TStyle.h"
+#include "TPad.h"
+#include "TGraph.h"
+#include "TAxis.h"
+
+using namespace std;
+
+void rms() {
+
+  TCanvas *c1 = new TCanvas();
+  
+  c1->SetFillColor(0);//45
+  c1->SetGrid();
+
+  int runNo;
+  cin >> runNo;
+  
+  TH1F *hist = new TH1F("hist", "", 16, 1.5, 1.9);
+  ifstream file;
+  file.open("TimingData_" + TString::Itoa(runNo,10) + ".txt");
+  double value;
+  int count = 0;
+  while(1)
+  {
+    if(file.eof()) break;
+    file >> value;
+    if (count) {
+    cout << value << endl;
+    hist->Fill(value);
+    }
+    count++;
+  }
+  file.close();
+  hist->GetYaxis()->SetTitle("Counts");
+  hist->GetXaxis()->SetTitle("FWHM");
+  hist->SetLineColor(kOrange-3);
+  hist->SetFillColor(kRed-4);
+  hist->Draw();
+  double rms;
+  rms = hist->GetRMS();
+  cout << "RMS is : " << rms << endl;
+  c1->Update();
+  c1->GetFrame()->SetFillColor(19);
+  c1->GetFrame()->SetBorderSize(12);
+  c1->Modified();
+  c1->Print("RMS_" + TString::Itoa(runNo,10) + ".png");
+  
+}
